4702e7c38d70bf702da3ac636630b600cdcf7154
leonlolleonlol <leonkoj2004@gmail.com>
2025-02-26 00:48:06 -0500

    Added avatar and first/last name fields to signup

--- communication-platform/client/src/pages/auth/index.jsx
+++ communication-platform/client/src/pages/auth/index.jsx
@@ -1,23 +1,28 @@
+import Avatar from '@/assets/avatars.png';
 import Cool from '@/assets/cool.svg';
 import Background from '@/assets/login.png';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Slider } from '@/components/ui/slider';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useState } from 'react';
-import { handleLogin, handleSignup } from './auth';
-import { useNavigate } from "react-router-dom";
 import { useUserStore } from '@/lib/store';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { handleLogin, handleSignup } from './auth';
 
 const Auth = () => {
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [confirmPassword,setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [sliderValue, setSliderValue] = useState(1); // Default value 50
+  const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate();
 
   const { setUserData } = useUserStore();
 
   // redirect the user to the chat landing page if sign-in is successful
-  const login = async () => { 
+  const login = async () => {
     let user = await handleLogin(email, password);
     if (user) {
       console.log(user);
@@ -37,12 +42,12 @@ const Auth = () => {
 
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <div className="h-[80vh] bg-amber-50 border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+      <div className=" h-[100vh] bg-amber-50 border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
         <div className="flex flex-col gap-10 items-center justify-center">
           <div className="flex items-center justify-center flex-col">
             <div className="flex items-center justify-center">
               <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
-              <img src={Cool} alt="Cool Emoji" className="h-[75px]"/>
+              <img src={Cool} alt="Cool Emoji" className="h-[75px]" />
             </div>
             <p className='font-medium text-center'>Create your account and let&apos;s get talkin&apos;!</p>
           </div>
@@ -50,53 +55,77 @@ const Auth = () => {
             <Tabs className="w-3/4">
               <TabsList className="bg-transparent rounded-none w-full">
                 <TabsTrigger
-                value='login'
-                className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:fontsemibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Login</TabsTrigger>
+                  value='login'
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:fontsemibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Login</TabsTrigger>
                 <TabsTrigger
-                value='signup'
-                className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:fontsemibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Signup</TabsTrigger>
+                  value='signup'
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:fontsemibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Signup</TabsTrigger>
               </TabsList>
               <TabsContent className="flex flex-col gap-5 mt-10" value="login">
                 <Input
-                placeholder="Email"
-                type="email"
-                className="rounded-full p-6"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <Input
-                placeholder="Password"
-                type="password"
-                className="rounded-full p-6"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <Button className="rounded-full p-6" onClick={(e) => login()}>
                   Login
                 </Button>
               </TabsContent>
-              <TabsContent className="flex flex-col gap-5"
-              value="signup">
+              <TabsContent className="flex flex-col gap-3"
+                value="signup">
+                  <img src={Avatar} alt="avatars" className="h-20" />
+                  <Slider
+                  value={[sliderValue]}
+                  onValueChange={(value) => setSliderValue(value[0])}
+                  min={1}
+                  max={6}
+                  step={1}
+                />
+
+                <p>Selected Avatar : #{sliderValue}</p>
                 <Input
-                placeholder="Email"
-                type="email"
-                className="rounded-full p-6"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                  placeholder="First Name"
+                  type="firstName"
+                  className="rounded-full p-6"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
                 />
                 <Input
-                placeholder="Password"
-                type="password"
-                className="rounded-full p-6"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Last Name"
+                  type="lastName"
+                  className="rounded-full p-6"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
                 />
                 <Input
-                placeholder="Confirm Password"
-                type="password"
-                className="rounded-full p-6"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                 />
                 <Button className="rounded-full p-6" onClick={(e) => signup()}>
                   Signup
